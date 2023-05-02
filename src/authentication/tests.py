619f0c6a2239b95dd32bcaee33cb70b6bbfdb8b5
ProtKsen <protcenko.kseniya@gmail.com>
2023-05-02 10:37:40 +0500

    Rename login tests

--- src/authentication/tests.py
+++ src/authentication/tests.py
@@ -3,14 +3,14 @@ from django.contrib import auth
 from django.urls import reverse
 
 
-def test_login_unauthorized_user_successed(client):
+def test_login_page_unauthorized_user_successed(client):
     url = reverse("login")
     response = client.get(url)
     assert response.status_code == 200
 
 
 @pytest.mark.django_db
-def test_login_authorized_user_redirect(client, create_user):
+def test_login_page_authorized_user_redirect(client, create_user):
     url = reverse("login")
     client.login(username="TestUser", password="TestPassword")
     response = client.get(url)
