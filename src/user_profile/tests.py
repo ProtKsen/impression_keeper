b6dea99bf8d4b98f7e41f69be1d07c2c7dfee61c
ProtKsen <protcenko.kseniya@gmail.com>
2023-05-03 12:20:31 +0500

    Add tests for user profile page

--- src/user_profile/tests.py
+++ src/user_profile/tests.py
@@ -1,3 +1,16 @@
-from django.test import TestCase
+import pytest
+from django.urls import reverse
 
-# Create your tests here.
+
+def test_user_profile_page_unauthorized_user_redirect(client):
+    url = reverse("user_profile")
+    response = client.get(url)
+    assert response.status_code == 302
+
+
+@pytest.mark.django_db
+def test_user_profile_page_authorized_user_successed(client, create_user):
+    url = reverse("user_profile")
+    client.login(username="TestUser", password="TestPassword")
+    response = client.get(url)
+    assert response.status_code == 200
