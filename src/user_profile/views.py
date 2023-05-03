b5a48a861ff280216720a0e02c9b0178529996bc
ProtKsen <protcenko.kseniya@gmail.com>
2023-05-03 13:30:47 +0500

    Add form handler

--- src/user_profile/views.py
+++ src/user_profile/views.py
@@ -4,6 +4,7 @@ from django.contrib.auth.decorators import login_required, user_passes_test
 from django.shortcuts import redirect, render
 
 from user_profile.forms import PlaceForm
+from user_profile.models import Place
 
 
 @login_required
@@ -16,7 +17,20 @@ def user_profile(request):
 @login_required
 def add_place(request):
     if request.method == "POST":
-        pass
+        form = PlaceForm(request.POST)
+        if form.is_valid():
+            form_data = form.cleaned_data
+
+            place_data = {}
+            for key, val in form_data.items():
+                if val:
+                    place_data[key] = val
+
+            place_data["user"] = request.user
+            place = Place(**place_data)
+            place.save()
+            return redirect("user_profile")
+
     form = PlaceForm()
     context = {"ymap_api_key": os.environ["YANDEX_MAPS_API_KEY"], "form": form}
     return render(request, "add_place.html", context)
