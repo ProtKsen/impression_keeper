1064ec45513ff573ab18b1d1ab6c332271c0112e
ProtKsen <protcenko.kseniya@gmail.com>
2023-05-03 11:25:47 +0500

    Add link to user's profile in header

--- src/user_profile/urls.py
+++ src/user_profile/urls.py
@@ -3,5 +3,5 @@ from django.urls import path
 from user_profile import views
 
 urlpatterns = [
-    path("", views.user_profile, name="userprofile"),
+    path("", views.user_profile, name="user_profile"),
 ]
