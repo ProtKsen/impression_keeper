9d6c5b401fdf02dba9259272a1159e59d2433a87
ProtKsen <protcenko.kseniya@gmail.com>
2023-05-03 11:54:58 +0500

    Create handler for adding places

--- src/user_profile/urls.py
+++ src/user_profile/urls.py
@@ -4,4 +4,5 @@ from user_profile import views
 
 urlpatterns = [
     path("", views.user_profile, name="user_profile"),
+    path("place/add", views.add_place, name="add_place"),
 ]
--- src/user_profile/views.py
+++ src/user_profile/views.py
@@ -1,5 +1,9 @@
+import os
+
 from django.contrib.auth.decorators import login_required, user_passes_test
 from django.shortcuts import redirect, render
+
+from user_profile.forms import PlaceForm
 
 
 @login_required
@@ -7,3 +11,12 @@ def user_profile(request):
     user = request.user
     context = {"user": user}
     return render(request, "user_profile.html", context=context)
+
+
+@login_required
+def add_place(request):
+    if request.method == "POST":
+        pass
+    form = PlaceForm()
+    context = {"ymap_api_key": os.environ["YANDEX_MAPS_API_KEY"], "form": form}
+    return render(request, "add_place.html", context)
