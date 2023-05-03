887b4debf3fd74c11e61cd11492496f86b6cbed1
ProtKsen <protcenko.kseniya@gmail.com>
2023-05-03 11:50:27 +0500

    Create model of place

--- src/user_profile/models.py
+++ src/user_profile/models.py
@@ -1,3 +1,13 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+
+class Place(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=100)
+    latitude = models.FloatField()
+    longitude = models.FloatField()
+    comment = models.CharField(max_length=1000, default="", blank=True)
+
+    def __str__(self):
+        return self.name
