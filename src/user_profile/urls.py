94777123378f1927a8d0923459324a59649504be
ProtKsen <protcenko.kseniya@gmail.com>
2023-05-04 22:36:13 +0500

    Add handlers for adding and editing places

--- src/user_profile/urls.py
+++ src/user_profile/urls.py
@@ -5,4 +5,6 @@ from user_profile import views
 urlpatterns = [
     path("", views.user_profile, name="user_profile"),
     path("place/add", views.add_place, name="add_place"),
+    path("place/edit/<int:id>/", views.edit_place, name="edit_place"),
+    path("place/delete/<int:id>/", views.delete_place, name="delete_place"),
 ]
